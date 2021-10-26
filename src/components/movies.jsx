c022f03b217453e1ee72d814a51c92ebefbff405
Duong Pham <ptduong987@gmail.com>
2021-10-26 21:41:03 +1100

    Comment link with action movie count

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -14,31 +14,37 @@ class Movies extends Component {
   }
   
   render() { 
+    const {length: count} = this.state.movies;
+    if(count === 0) return <p>There are no movie on the database</p>
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Number In Stock</th>
-            <th>Daily Rental Rate</th>
-            {/* Empty table header for Delete */}
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(movie => (
-              <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td><button onClick={() => this.handleDelete(movie)}className="btn btn-danger btn-sm">Delete</button></td>
-            </tr>
-          ))}
-        
-        </tbody>
-      </table>
+      <>
+            <p>Showing {this.state.movies.length} movies in the database</p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Genre</th>
+                  <th>Number In Stock</th>
+                  <th>Daily Rental Rate</th>
+                  {/* Empty table header for Delete */}
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.movies.map(movie => (
+                    <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td><button onClick={() => this.handleDelete(movie)}className="btn btn-danger btn-sm">Delete</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+      </>
+      
     )
   }
 }
